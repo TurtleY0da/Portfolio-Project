3795fdc8d1e7a8984f30675f5b808618b5000fb9
TurtleY0da <timothy.veugelers@gmail.com>
2021-12-06 14:48:00 -0700

    Tech Tree: Adding Tree item creator

--- techTree/script.js
+++ techTree/script.js
@@ -8,7 +8,7 @@ let cnv = docGetID("treeCanvas");
 let dBoxEl = docGetID('dialogueBox');
 dialogPolyfill.registerDialog(dBoxEl);
 
-let closeModalEl = docGetID('closeModalBtn');
+let dialogBtnContainer = docGetID('divBtnContainer');
 
 // Glbl Variables
 let screenSize = {
@@ -42,9 +42,14 @@ let cousine = new FontFace('cousine', 'url(../fonts/Cousine-Regular.ttf)');
 let topTreeElements = new Array();
 
 let scrolling = false;
-let bounds = new boundingBox(-100, -100, 300, 300)
+
+let bounds = new boundingBox(-100, -100, 300, 300);
+
 let smallestEdge = Math.min(cnv.width, cnv.height);
-let buttonVertices = [new PVector(0, 0), new PVector(smallestEdge/12, 0), new PVector(smallestEdge/12, smallestEdge/12-smallestEdge/12/6), new PVector(smallestEdge/12-smallestEdge/12/6, smallestEdge/12), new PVector(0, smallestEdge/12)];
+
+let buttonVertices = [new PVector(0, 0), new PVector(smallestEdge / 12, 0), new PVector(smallestEdge / 12, smallestEdge / 12 - smallestEdge / 12 / 6), new PVector(smallestEdge / 12 - smallestEdge / 12 / 6, smallestEdge / 12), new PVector(0, smallestEdge / 12)];
+
+let buttonHover = false;
 
 let drawPromise;
 let drawing = false;
@@ -79,6 +84,7 @@ function loop() {
         cnv.width = Math.max(screenSize.width / 1.5);
         cnv.height = Math.max(screenSize.height / 1.5);
         smallestEdge = Math.min(cnv.width, cnv.height);
+        buttonVertices = [new PVector(0, 0), new PVector(smallestEdge / 12, 0), new PVector(smallestEdge / 12, smallestEdge / 12 - smallestEdge / 12 / 6), new PVector(smallestEdge / 12 - smallestEdge / 12 / 6, smallestEdge / 12), new PVector(0, smallestEdge / 12)];
     }
 
     cnv.width = Math.max(screenSize.width / 1.5);
@@ -96,6 +102,10 @@ function loop() {
 
     ctx.fillStyle = '#FF2E63';
     ctx.strokeStyle = '#EEEEEE';
+    if (buttonHover) {
+        ctx.fillStyle = '#EEEEEE';
+        ctx.strokeStyle = '#FF2E63';
+    }
     ctx.lineWidth = smallestEdge / 100;
     ctx.lineCap = 'round';
 
@@ -129,7 +139,7 @@ cnv.oncontextmenu = function (e) {
     e.preventDefault();
 }
 
-closeModalEl.addEventListener('click', closeModal);
+dialogBtnContainer.children[0].children[0].addEventListener('click', closeModal);
 cnv.addEventListener('wheel', scrollHandler);
 cnv.addEventListener('mousedown', clickHandler);
 cnv.addEventListener('mouseup', clickHandler);
@@ -142,21 +152,26 @@ cnv.addEventListener('mousemove', moveHandler);
 function closeModal() {
     dBoxEl.classList.remove('opening');
     dBoxEl.classList.add('closing');
-    dBoxEl.addEventListener('animationend', function (event) {
-        if (dBoxEl.classList[0] === 'closing') {
-            dBoxEl.close();
-            dBoxEl.classList.remove('closing');
+    dBoxEl.addEventListener('animationend', modalClosed);
+}
+
+function modalClosed() {
+    if (dBoxEl.classList[0] === 'closing') {
+        dBoxEl.close();
+        dBoxEl.classList.remove('closing');
+    }
+    console.log(event);
+    let removalArray = new Array();
+    for (const child of dBoxEl.children) {
+        removalArray.push(child);
+    }
+    removalArray.forEach((element, index) => {
+        console.log('ran');
+        if (index > 0) {
+            element.remove();
         }
-        let removalArray = new Array();
-        for (const child of dBoxEl.children) {
-            removalArray.push(child);
-        }
-        removalArray.forEach((element, index) => {
-            if (index > 0) {
-                element.remove();
-            }
-        });
     });
+    dBoxEl.removeEventListener('animationend', modalClosed)
 }
 
 function openModal() {
@@ -184,9 +199,13 @@ function scrollHandler(event) {
 function clickHandler(event) {
     switch (event.type) {
         case 'mousedown':
-            if (clickHeld !== event.button && clickHeld !== -1 || event.button === 1) break;
-            clickHeld = event.button;
-            beginDrag(event);
+            if (clickHeld !== event.button && clickHeld !== -1 || event.button === 1) break; 
+            if(buttonHover){
+                createTopLevel();
+            } else {
+                clickHeld = event.button;
+                beginDrag(event);
+            }
             break;
         case 'mouseup':
             if (clickHeld !== event.button) break;
@@ -198,14 +217,16 @@ function clickHandler(event) {
 }
 
 function moveHandler(event) {
+    cnvRect = cnv.getBoundingClientRect();
     if (clickHeld > -1) {
-        cnvRect = cnv.getBoundingClientRect();
         switch (clickHeld) {
             case 2:
                 dragOffset.x = (dragStart.x - (event.clientX - cnvRect.left))
                 dragOffset.y = (dragStart.y - (event.clientY - cnvRect.top))
                 break;
         }
+    } else {
+        checkHover(event.clientX - cnvRect.left, event.clientY - cnvRect.top);
     }
 }
 
@@ -238,10 +259,8 @@ function endDrag() {
 function checkHover(mouseX, mouseY) {
     // Check for button hover
     // If false, check for hover on 'treeItem's
-    // if (polyPoint(buttonVertices), mouseX, mouseY){
-    //     console.log(true);
-    // }
-    // else console.log(false);
+    if (polyPoint(buttonVertices, mouseX, mouseY)) buttonHover = true;
+    else buttonHover = false;
 }
 
 function finishedDraw() {
@@ -267,4 +286,45 @@ function getWrappedLines(textArray, size, preferedLineSize) {
         if (measureWidth(lines[lines.length - 2], size) + elementWidth < preferedLineSize) lines.mergeStrings(lines.length - 2, 2, "");
     });
     return lines;
+}
+
+async function createTopLevel() {
+    dialogBtnContainer.children[1].classList.add('displayNone');
+    dialogBtnContainer.children[2].classList.add('displayNone');
+
+    let titleTitle = document.createElement('h2');
+    let titleInput = document.createElement('input');
+
+    let descriptionTitle = document.createElement('h3');
+    let descriptionInput = document.createElement('textarea');
+
+    let costTitle = document.createElement('h3');
+    let costInput = document.createElement('input');
+
+    let lb = document.createElement('br');
+
+    let submitButton = document.createElement('button');
+
+    titleInput.type = 'text';
+    descriptionInput.rows = '10';
+    descriptionInput.cols = '50';
+    costInput.type = 'number';
+    costInput.min = '0';
+    costInput.max = '9999';
+    costInput.onkeydown = function () {
+        return false;
+    };
+
+    titleTitle.innerText = "Item Title";
+    descriptionTitle.innerText = "Item Description";
+    costTitle.innerText = "Item Cost";
+
+    dBoxEl.append(titleTitle, titleInput, descriptionTitle, descriptionInput, costTitle, costInput);
+    openModal();
+
+    // await submitButton();
+}
+
+async function submitButton(submitButton) {
+
 }
